ddc1cfbac1d0f32761051d83a66121a588b3a0d6
kellraym <ray.al.kell@gmail.com>
2023-08-04 11:56:32 -0600

    finished table create statements function

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pandas as pd
 import os
 
 
+class DataFrameNamePair:
+    """Stores a table dataframe with its original excel filename"""
+    def __init__(self, data_frame: pd.DataFrame, name: str):
+        self.data_frame = data_frame
+        self.name = name
+
+
 def get_all_dataframes(folder_name: str):
     """
     Creates dataframes from all excel files in target folder
@@ -13,20 +20,40 @@ def get_all_dataframes(folder_name: str):
         all_dataframes = []
         for filename in all_excels:
             try:
-                all_dataframes.append(pd.read_excel(f"./{folder_name}/{filename}"))
+                df_pair = DataFrameNamePair(
+                    pd.read_excel(f"./{folder_name}/{filename}"),
+                    filename.split(".")[0]
+                )
+                all_dataframes.append(df_pair)
             except IsADirectoryError:
                 continue
 
         return all_dataframes
     except FileNotFoundError:
         print(f"folder {folder_name} not found")
-        return
+        raise FileNotFoundError
 
 
-def build_sql_config(folder_name: str):
-    print(get_all_dataframes(folder_name))
+def build_create_table(table_pair: DataFrameNamePair) -> str:
+    column_strings = []
+    table_name, table_frame = table_pair.name, table_pair.data_frame
+    for column, datatype in zip(table_frame.columns, table_frame.dtypes):
+        new_name = "_".join(column.casefold().split(" "))
+        column_strings.append(f"{new_name} {sql_types[str(datatype)]}")
+
+    joined_column_strings = ",\n".join(column_strings)
+    return f"CREATE TABLE {table_name} (\n{joined_column_strings}\n);"
+
+
+def build_sql_statements(folder_name: str):
+    all_dataframes = get_all_dataframes(folder_name)
+    for table_pair in all_dataframes:
+        build_create_table(table_pair)
 
 
 if __name__ == '__main__':
-    build_sql_config("./RespondDB")  # TODO: make this take a command line arg
+    try:
+        build_sql_statements("./RespondD")  # TODO: make this take a command line arg
+    except FileNotFoundError:
+        pass
 
